9749dadc7a234290f20b77370116bf7805016933
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-03-17 17:23:27 +0900

    Get and parse package list

--- dragon/smaug_client.rb
+++ dragon/smaug_client.rb
@@ -4,131 +4,77 @@
 
 module GTK
   class SmaugClient
+    class Package
+      class Version
+        def self.parse_version_string(version)
+          version.split('.').map(&:to_i)
+        end
+
+        # 2021-03-11T14:23:37.289Z
+        def self.parse_timestamp(timestamp)
+          Time.new(
+            timestamp[0..3].to_i, # Year
+            timestamp[5..6].to_i, # Month
+            timestamp[8..9].to_i, # Day
+            timestamp[11..12].to_i, # Hour
+            timestamp[14..15].to_i, # Minute
+            timestamp[17..18].to_i, # Second
+          )
+        rescue ArgumentError
+          puts "Invalid timestamp #{timestamp}"
+        end
+
+        attr_reader :author, :description, :created_at, :version
+
+        def initialize(json)
+          @authors = json['authors']
+          @description = json['description']
+          @created_at = Version.parse_timestamp(json['created_at'])
+          @repository = json['repository']
+          @version = Version.parse_version_string(json['version'])
+        end
+
+        def to_s
+          @version.map(&:to_s).join('.')
+        end
+      end
+
+      attr_reader :name, :versions
+
+      def initialize(json)
+        @name = json['name']
+        @versions = json['versions'].map { |version_json| Version.new(version_json) }
+      end
+
+      def author
+        latest_version.author
+      end
+
+      def description
+        latest_version.description
+      end
+
+      def last_updated
+        latest_version.created_at
+      end
+
+      def latest_version
+        @latest_version ||= @versions.max_by { |version| version.version }
+      end
+
+      def inspect
+        "Package '#{@name}' (latest version #{latest_version}, last updated: #{last_updated})"
+      end
+    end
+
     class << self
-      def list_packages
-        # FIXME: $gtk.http_get 'https://api.smaug.dev/packages'
-        [
-          {
-            "name" => "color",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "https://gitlab.com/ereborstudios/color.git",
-                  "tag" => "0.1.2"
-                },
-                "version" => "0.1.2",
-                "authors" => ["Logan Koester \u003clogan@logankoester.com\u003e"],
-                "description" => "Color manipulation utilities for DragonRuby",
-                "created_at" => "2021-03-11T08:35:47.896Z"
-              }
-            ]
-          },
-          {
-            "name" => "draco",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "git://github.com/guitsaru/draco.git",
-                  "tag" => "v0.6.1"
-                },
-                "version" => "0.6.1",
-                "authors" => ["Matt Pruitt \u003cmatt@guitsaru.com\u003e"],
-                "description" => "An Entity Component System for DragonRuby GTK",
-                "created_at" => "2021-03-11T09:43:08.779Z"
-              }
-            ]
-          },
-          {
-            "name" => "draco-common",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "https://gitlab.com/ereborstudios/draco-common.git",
-                  "tag" => "0.1.1"
-                },
-                "version" => "0.1.1",
-                "authors" => ["Logan Koester \u003clogan@logankoester.com\u003e"],
-                "description" => "This package adds some common components and systems to your Draco project to help you get started.",
-                "created_at" => "2021-03-11T08:40:35.131Z"
-              }
-            ]
-          },
-          {
-            "name" => "draco-events",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "git://github.com/guitsaru/draco-events.git",
-                  "tag" => "v0.2.0"
-                },
-                "version" => "0.2.0",
-                "authors" => ["Matt Pruitt \u003cmatt@guitsaru.com\u003e"],
-                "description" => "An event bus and observer plugin for the Draco ECS library.",
-                "created_at" => "2021-03-11T14:07:30.734Z"
-              }
-            ]
-          },
-          {
-            "name" => "draco-periodic",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "git://github.com/guitsaru/draco-periodic.git",
-                  "tag" => "v0.2.0"
-                },
-                "version" => "0.2.0",
-                "authors" => ["Matt Pruitt \u003cmatt@guitsaru.com\u003e"],
-                "description" => "Run a Draco System every n ticks",
-                "created_at" => "2021-03-11T14:13:22.049Z"
-              }
-            ]
-          },
-          {
-            "name" => "draco-scenes",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "git://github.com/guitsaru/draco-scenes.git",
-                  "tag" => "v0.2.0"
-                },
-                "version" => "0.2.0",
-                "authors" => ["Matt Pruitt \u003cmatt@guitsaru.com\u003e"],
-                "description" => "A scene definition DSL for the Draco ECS library.",
-                "created_at" => "2021-03-11T14:16:28.074Z"
-              }
-            ]
-          },
-          {
-            "name" => "draco-state",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "git://github.com/guitsaru/draco-state.git",
-                  "tag" => "v0.2.0"
-                },
-                "version" => "0.2.0",
-                "authors" => ["Matt Pruitt \u003cmatt@guitsaru.com\u003e"],
-                "description" => "A State management implementation for the Draco ECS framework.",
-                "created_at" => "2021-03-11T14:23:37.289Z"
-              }
-            ]
-          },
-          {
-            "name" => "zif",
-            "versions" => [
-              {
-                "repository" => {
-                  "url" => "git://github.com/logankoester/dragonruby-zif.git",
-                  "tag" => "smaug"
-                },
-                "version" => "2.0.0-rc.2",
-                "authors" => ["Dan Healy \u003cdan@beyondludus.com\u003e"],
-                "description" => "Zif: A Drop-in Framework for DragonRuby GTK",
-                "created_at" => "2021-03-11T17:29:15.698Z"
-              }
-            ]
-          }
-        ]
+      def get_packages_json_from_api
+        $gtk.parse_json `curl https://api.smaug.dev/packages` # FIXME: $gtk.http_get 'https://api.smaug.dev/packages'
+      end
+
+      def get_packages_from_api
+        packages_json = get_packages_json_from_api
+        packages_json.map { |package_json| Package.new(package_json) }
       end
     end
   end
