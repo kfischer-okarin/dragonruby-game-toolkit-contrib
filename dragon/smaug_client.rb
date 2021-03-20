d2736faa0134c5e2a560dd9cdd067d92ccb6426e
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-03-21 08:05:35 +0900

    Remove source classes since we will be using smaug

--- dragon/smaug_client.rb
+++ dragon/smaug_client.rb
@@ -4,54 +4,6 @@
 
 module GTK
   class SmaugClient
-    class GithubSource
-      GITHUB_PREFIX = 'git://github.com/'.freeze
-      GITHUB_SUFFIX = '.git'.freeze
-
-      def self.applicable?(version_json)
-        version_json.dig('repository', 'url')&.start_with? GITHUB_PREFIX
-      end
-
-      def initialize(version_json)
-        repository_json = version_json['repository']
-
-        # git://github.com/user/repository.git -> https://codeload.github.com/user/repository/zip/tag
-        @download_url = 'https://codeload.github.com/' +
-                        repository_json['url'][GITHUB_PREFIX.size...-GITHUB_SUFFIX.size] +
-                        '/zip/' +
-                        repository_json['tag']
-      end
-
-      def to_s
-        "Github(#{@download_url})"
-      end
-    end
-
-    class GitlabSource
-      def self.applicable?(version_json)
-        version_json.dig('repository', 'url')&.start_with? 'https://gitlab.com'
-      end
-
-      def self.repository_name(repository_url)
-        last_slash_index = repository_url.rindex('/')
-        repository_url[(last_slash_index + 1)..-1]
-      end
-
-      def initialize(version_json)
-        repository_json = version_json['repository']
-
-        repository_url = repository_json['url']
-        tag = repository_json['tag']
-        repository_name = GitlabSource.repository_name(repository_url)
-        # https://gitlab.com/ereborstudios/color -> https://gitlab.com/ereborstudios/color/-/archive/0.1.2/color-0.1.2.zip
-        @download_url = repository_url + "/-/archive/#{tag}/#{repository_name}-#{tag}.zip"
-      end
-
-      def to_s
-        "Gitlab(#{@download_url})"
-      end
-    end
-
     class Package
       class Version
         def self.parse_version_string(version)
@@ -72,14 +24,12 @@ module GTK
           puts "Invalid timestamp #{timestamp}"
         end
 
-        attr_reader :author, :description, :created_at, :version, :source
+        attr_reader :author, :description, :created_at, :version
 
         def initialize(json)
           @authors = json['authors']
           @description = json['description']
           @created_at = Version.parse_timestamp(json['created_at'])
-          source_class = [GithubSource, GitlabSource].find { |klass| klass.applicable? json }
-          @source = source_class.new(json)
           @version = Version.parse_version_string(json['version'])
         end
 
