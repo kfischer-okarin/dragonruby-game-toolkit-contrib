b535a0aeff9976a79a7319172afb41e7fd87ba45
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-09 15:02:44 +0900

    Implement Tokenizer for Doc Parsing

--- dragon/doc_parser.rb
+++ dragon/doc_parser.rb
@@ -44,5 +44,50 @@ module GTK
       elements << text if text.length > 0
       elements
     end
+
+    private
+
+    class Tokenizer
+      attr_reader :tokens
+
+      def initialize(doc_string)
+        @chars = doc_string.chars
+        @index = 0
+        @tokens = []
+        @current_text = ''
+
+        tokenize
+      end
+
+      def tokenize
+        while @index < @chars.length
+          char = @chars[@index]
+
+          case char
+          when '*'
+            finish_text
+            @tokens << :h1
+            @index += 1
+          when '~'
+            finish_text
+            @tokens << :tilde
+          when "\n"
+            finish_text
+            @tokens << :newline
+          else
+            @current_text << char
+          end
+
+          @index += 1
+        end
+      end
+
+      def finish_text
+        return if @current_text.empty?
+
+        @tokens << @current_text
+        @current_text = ''
+      end
+    end
   end
 end
--- samples/10_advanced_debugging/03_unit_tests/api_doc_parsing_tests.rb
+++ samples/10_advanced_debugging/03_unit_tests/api_doc_parsing_tests.rb
@@ -1,3 +1,23 @@
+def test_doc_parse_tokenize(args, assert)
+  tokens = GTK::DocParser::Tokenizer.new(<<~S).tokens
+    * DOCS: ~GTK::Args#audio~
+
+    Audio docs
+  S
+
+  assert.equal! tokens, [
+    :h1,
+    'DOCS: ',
+    :tilde,
+    'GTK::Args#audio',
+    :tilde,
+    :newline,
+    :newline,
+    'Audio docs',
+    :newline
+  ]
+end
+
 def test_parse_header_code_text(args, assert)
   elements = GTK::ApiDocExport.parse_doc_entry <<~S
     * DOCS: ~GTK::Args#audio~
